05b873b42fdd06dbcf6c85127a39e0619d6d8ca1
Frank-Bruno <f.bruno227@gmail.com>
2024-12-18 19:22:39 -0300

    consumer-node

--- consumer_node/receive-c.py
+++ consumer_node/receive-c.py
@@ -1,17 +1,55 @@
-#!/usr/bin/env python
-import pika, sys, os
+"""
+MQTT subscriber - Listen to a topic and sends data to InfluxDB
+"""
 
+import os
+from influxdb_client import InfluxDBClient, Point
+import struct
+import json
+import pika, sys, os
 import pika.credentials
 
-def main():
-    cred = pika.credentials.PlainCredentials("rabbitmq","rabbitmq")
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost",credentials=cred))
-    channel = connection.channel()
+# Configuração do InfluxDB
+BUCKET = "my-bucket"  # Nome do BUCKET no InfluxDB
+ORG = "my-org"  # Nome da organização no InfluxDB
+TOKEN ="my-super-secret-token"  # Token de acesso do InfluxDB
+URL = "http://influxdb-influxdb2:8086"  # URL do servidor InfluxDB
 
-    #channel.queue_declare(queue='teste1')
+client = InfluxDBClient(url=URL, token=TOKEN, org=ORG)
+write_api = client.write_api()
+
+# AMQP broker config
+AMQP_BROKER_URL    = "rabbitmq-headless" 
+
+
+def on_message(msg):
+    """ The callback for when a PUBLISH message is received from the server."""
+    # Decodifica o payload de bytes para string
+    payload_str = msg.decode('utf-8')
+    #test = json.dumps(payload_str)
+    values = json.loads(payload_str)
+
+    #print(type(values))
+    #print(values)
+
+    ## InfluxDB logic
+    point = (
+        Point(BUCKET)
+        .tag("node_id", values["node_id"])
+        .field("humidity", values["humidity"])
+        .field("temperature", values["temperature"])
+        .field("timestamp", values["timestamp"])
+    )
+    write_api.write(bucket=BUCKET, record=point)
+
+def main():
+    cred = pika.credentials.PlainCredentials("admin","admin")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=AMQP_BROKER_URL,credentials=cred))
+    channel = connection.channel()
 
     def callback(ch, method, properties, body):
         print(f" [x] Received {body}")
+        on_message(body)
 
     channel.basic_consume(queue='c', on_message_callback=callback, auto_ack=True)
 
